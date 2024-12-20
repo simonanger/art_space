0e44940bed9289b7022d9fd1788d2eb73733b771
Simon Anger <simonanger89@gmail.com>
2024-12-20 08:11:41 +0000

    have built basic structure of app

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtSpaceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ArtworkWall(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun ArtworkWall(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.main_image)
+    Box(modifier) {
+        Column {
+            Image(
+                painter = image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(9.dp)
+            )
+            ArtworkDescriptor(
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Buttons(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+@Composable
+fun ArtworkDescriptor(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 32.sp,
+            modifier = Modifier
+                .padding(6.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(R.string.photographer),
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(6.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun Buttons(modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+    Row(modifier = modifier) {
+        Button(onClick = {if(result > 1){result -1}}) {
+            Text(stringResource(R.string.previous))
+        }
+        Spacer(modifier = Modifier.padding(6.dp))
+        Button(onClick = { result +1 }) {
+            Text(stringResource(R.string.next))
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtSpaceTheme {
+        Greeting("Android")
+    }
+}
